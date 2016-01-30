cd945ea72291e2b0a0041c7a3bea245608175bdc
Keith Lackey <keith@steamrollerstudios.com>
2016-01-30 17:07:07 -0500

    did a bit of house cleaning and expose some variables

--- Assets/Steamroller/Content/Players/GenericMovement.cs
+++ Assets/Steamroller/Content/Players/GenericMovement.cs
@@ -2,154 +2,154 @@
 using System.Collections;
 using Steamroller.Objects;
 
-
 namespace Steamroller.Characters
-{
-    
-    public class GenericMovement : MonoBehaviour
+{   
+    public class GenericMovement : CachedMonoBehaviour
     {
         //we dont want to control direction of the force
         public float speed;
-        public float IntersectionThreshold;
-        private bool Orbiting = false;
-
-        public float orbitSpeed = 1.0f;
-        public float orbitDistance;
-
-        private Oribitable Interactable;
 
         public Vector3 selfPoint;
-        public Vector3 targetPoint;
-        public Vector3 targetVector;
-        public float targetDistance;
-        public float targetAngle;
+
+        public float orbitSpeed = 1.0f;
+        public float orbitRadius;
+        public Vector3 orbitPoint;
+        public float orbitDistance;
+        public float orbitThreshold;
+        public float orbitSide = 1.0f;
+        public bool orbiting = false;
+        public bool orbit = false;
+
+        public Oribitable orbitable;
+
+        public Vector3 orbitablePoint;
+        public Vector3 orbitableVector;
+        public float orbitableDistance;
+        public float orbitableAngle;
         
-        public Vector3 intersectionPoint;
-        public float intersectionDistance;
-        public float directionMultiplier = 1.0f;
-         
-        void Awake()
+        protected override void Start()
         {
+            orbit = true;
+            FindOrbitable();
+            FindOrbitPoint();
         }
 
-        
-        void Start()
+        protected override void Update()
         {
-            FindTarget();
-        }
+            FindOrbitable();
 
-        
-        void Update()
-        {
             HandleInput();
 
-            if (Interactable)
+            if ( orbitable )
             {
-                if (Orbiting)
+                if ( orbiting )
                 {
-                    transform.RotateAround(Interactable.transform.position, Vector3.forward, directionMultiplier * (1.0f / orbitDistance) * orbitSpeed * Time.deltaTime);
+                    transform.RotateAround(orbitable.transform.position, Vector3.forward, orbitSide * (1.0f / orbitRadius) * orbitSpeed * Time.deltaTime);
                     return;
                 }
                 else
                 {
-                    float _distToIntersection = (intersectionPoint - transform.position).sqrMagnitude;
-                    if (IntersectionThreshold * IntersectionThreshold > _distToIntersection)
+                    if ( orbit )
                     {
-                        Orbiting = true;
-                        return;
+                        float _distanceToOrbit = (orbitPoint - transform.position).sqrMagnitude;
+                        if ( _distanceToOrbit < orbitThreshold * orbitThreshold )
+                        {
+                            orbiting = true;
+                            return;
+                        }
                     }
                 }
             }
             
-
-            float _speedStep = speed * Time.deltaTime;
-            transform.Translate(new Vector3(0.0f, _speedStep, 0.0f), Space.Self);
+            transform.Translate(Vector3.up * speed * Time.deltaTime, Space.Self);
         }
 
-
-        void HandleInput()
+        private void HandleInput()
         {
+            if ( InputManager.GetPressed( ActionType.Orbit ) )
+            {
+                orbit = false;
+                orbiting = false;
+            }
+
             if (InputManager.GetReleased(ActionType.Orbit) )
             {
-                FindTarget();
-            }
-            else if (!InputManager.GetState(ActionType.Orbit))
-            {
-
-            }
-            else if (InputManager.GetPressed(ActionType.Orbit))
-            {
-                Interactable = null;
-                Orbiting = false;
+                orbit = true;
+                FindOrbitPoint();
             }
         }
 
-        void FindTarget()
+        private void FindOrbitable()
         {
-            float closestDist = float.MaxValue;
-            foreach (var item in GameObject.FindGameObjectsWithTag("Interactable"))
+            if ( orbiting )
             {
-
-
-
-                Vector3 _dist = item.transform.position - transform.position;
-                //rule out anyting behind me
-                if (Vector3.Dot(transform.up, _dist) < 0.0f)
-                {
-                    continue;
-                }
-
-                if (_dist.sqrMagnitude < closestDist)
-                {
-
-                    Interactable = item.GetComponent<Oribitable>();
-                    closestDist = _dist.sqrMagnitude;
-                }
-            }
-
-            if (Interactable == null)
-            {
-                Orbiting = false;
                 return;
             }
 
             selfPoint = transform.position;
-            targetPoint = Interactable.transform.position;
 
-            //grab the vector from the orbitable to me
-            targetVector = targetPoint - selfPoint;
+            orbitable = null;
 
-            //get the distance
-            targetDistance = targetVector.magnitude;
+            float _closestDist = float.MaxValue;
+            foreach ( var _oribitable in GameObject.FindObjectsOfType<Oribitable>() )
+            {
+                var _distance = _oribitable.transform.position - selfPoint;
 
-            directionMultiplier = Vector3.Dot(transform.right, targetVector) < 0.0f ? 1.0f : -1.0f;
+                //rule out anyting behind me
+                if ( Vector3.Dot( transform.up, _distance ) < 0.0f )
+                {
+                    continue;
+                }
 
-            //get the angle between the two
-            targetAngle = Vector3.Angle(targetVector, transform.up);
-
-            orbitDistance = Mathf.Sin(targetAngle * Mathf.Deg2Rad) * targetDistance;
-
-            intersectionDistance = Mathf.Sqrt((targetDistance * targetDistance) - (orbitDistance * orbitDistance));
-            intersectionPoint = transform.TransformPoint(Vector3.up * intersectionDistance);
+                if ( _distance.sqrMagnitude < _closestDist )
+                {
+                    orbitable = _oribitable;
+                    _closestDist = _distance.sqrMagnitude;
+                }
+            }
         }
 
-      
-            void OnDrawGizmos()
+        private void FindOrbitPoint()
+        {
+            if ( orbitable == null )
             {
-                Gizmos.color = Color.red;
-                Gizmos.DrawRay(transform.position, transform.up);
-
-                Gizmos.color = Color.yellow;
-
-	            Gizmos.DrawLine(selfPoint, targetPoint);
-                Gizmos.color = Color.green;
-                Gizmos.DrawLine(selfPoint, intersectionPoint);
-                Gizmos.color = Color.yellow;
-                GizmosUtils.DrawCircle(targetPoint, orbitDistance, 24);
-                Gizmos.DrawSphere(intersectionPoint, 0.25f);
-                
-
-
+                return;
             }
+
+            selfPoint = transform.position;
+
+            orbitablePoint = orbitable.transform.position;
+
+            //grab the vector from the orbitable to me
+            orbitableVector = orbitablePoint - selfPoint;
+
+            //get the distance
+            orbitableDistance = orbitableVector.magnitude;
+
+            orbitSide = Vector3.Dot(transform.right, orbitableVector) < 0.0f ? 1.0f : -1.0f;
+
+            //get the angle between the two
+            orbitableAngle = Vector3.Angle(orbitableVector, transform.up);
+
+            orbitRadius = Mathf.Sin(orbitableAngle * Mathf.Deg2Rad) * orbitableDistance;
+
+            orbitDistance = Mathf.Sqrt((orbitableDistance * orbitableDistance) - (orbitRadius * orbitRadius));
+            orbitPoint = transform.TransformPoint(Vector3.up * orbitDistance);
+        }
+      
+        protected void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, transform.up);
+
+            Gizmos.color = Color.yellow;
+
+	        Gizmos.DrawLine(selfPoint, orbitablePoint);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(selfPoint, orbitPoint);
+            Gizmos.color = Color.yellow;
+            GizmosUtils.DrawCircle(orbitablePoint, orbitRadius, 24);
+            Gizmos.DrawSphere(orbitPoint, 0.25f);
+        }
     } 
 }
