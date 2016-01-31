b5de278f5c58b76abcaf1942cb7871412071f06b
Keith Lackey <keith@steamrollerstudios.com>
2016-01-31 11:15:59 -0500

    Added GetCurrentPlayer and SetNextPlayer to PlayerManager

---
+++ Assets/Steamroller/Systems/Extensions/IntExtensions.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Steamroller.Extensions
+{
+    public static class IntExtensions
+    {
+        private static int difference;
+        private static int result;
+
+        public static int Wrap( this int value, int min, int max )
+        {
+            result = value;
+
+            difference = max - min;
+            if ( difference <= 0 )
+            {
+                return value;
+            }
+
+            while ( result < min )
+            {
+                result += difference;
+            }
+
+            while ( result >= max )
+            {
+                result -= difference;
+            }
+
+            return result;
+        }
+
+        public static int Wrap( this int value, int max )
+        {
+            return value.Wrap( 0, max );
+        }
+    }
+}
--- Assets/Steamroller/Systems/PlayerSystem/Scripts/PlayerManager.cs
+++ Assets/Steamroller/Systems/PlayerSystem/Scripts/PlayerManager.cs
@@ -1,10 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
+using Steamroller.Extensions;
 
 namespace Steamroller
 {
     public class PlayerManager : SingletonMonoBehaviour<PlayerManager>
     {
         public Player[] players;
+        public Player currentPlayer;
+
+        public static Player GetCurrentPlayer()
+        {
+            return instance.currentPlayer;
+        }
+
+        public static Player SetNextPlayer()
+        {
+            if ( instance.currentPlayer != null )
+            {
+                for ( var _index = 0; _index < instance.players.Length; _index++ )
+                {
+                    var _player = instance.players[ _index ];
+                    if ( _player == instance.currentPlayer )
+                    {
+                        _index += 1;
+                        instance.currentPlayer = instance.players[ _index.Wrap( instance.players.Length ) ];
+                        break;
+                    }
+                }
+            }
+
+            return GetCurrentPlayer();
+        }
     }
 }
